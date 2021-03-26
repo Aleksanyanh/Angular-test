41efc562adef5986728b646b2e5fa445530ee83b
Aleksanyanh <aleksanyanhayk91@gmail.com>
2021-03-26 18:09:10 +0400

    created users repo service

---
+++ src/app/core/repos/users.repo.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { IResponseData } from '@app/core/models/response.model';
+import { IUsersResDTO } from '@app/features/models/users.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersRepoService {
+  constructor(private http: HttpClient) {}
+
+  getUsersRepo(params: string) {
+    const url = `/search/users?${params}`;
+    return this.http.get<IResponseData<IUsersResDTO>>(url);
+  }
+}
